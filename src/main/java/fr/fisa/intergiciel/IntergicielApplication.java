270844f53b8cab988defa0fa9dc3bf4e2aad5db9
Léo Wadin <wadinleo@gmail.com>
2024-02-09 10:32:38 +0100

    Spring Init

---
+++ src/main/java/fr/fisa/intergiciel/IntergicielApplication.java
@@ -0,0 +1,13 @@
+package fr.fisa.intergiciel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IntergicielApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IntergicielApplication.class, args);
+	}
+
+}
