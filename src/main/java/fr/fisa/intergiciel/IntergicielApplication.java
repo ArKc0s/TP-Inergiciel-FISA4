939e0a55eddd572f2bb5b233698e2e792065fc97
Léo Wadin <wadinleo@gmail.com>
2024-02-22 14:49:35 +0100

    feat : prod/cons (WIP)

--- src/main/java/fr/fisa/intergiciel/IntergicielApplication.java
+++ src/main/java/fr/fisa/intergiciel/IntergicielApplication.java
@@ -1,9 +1,11 @@
 package fr.fisa.intergiciel;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.kafka.core.KafkaTemplate;
 
-@SpringBootApplication
+@SpringBootApplication(scanBasePackages = {"fr.fisa.intergiciel"})
 public class IntergicielApplication {
 
 	public static void main(String[] args) {
