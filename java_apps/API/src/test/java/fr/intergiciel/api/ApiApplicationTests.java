b9d696b117b1c71588d1eb9016c4e2b6fb297a01
Léo Wadin <wadinleo@gmail.com>
2024-02-23 10:30:57 +0100

    Reworked file structure

--- java_apps/API/src/test/java/fr/intergiciel/api/ApiApplicationTests.java
+++ java_apps/API/src/test/java/fr/intergiciel/api/ApiApplicationTests.java
@@ -1,12 +1,10 @@
-package fr.fisa.intergiciel;
+package fr.intergiciel.api;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
-class IntergicielApplicationTests {
+class ApiApplicationTests {
 
 	@Test
 	void contextLoads() {
