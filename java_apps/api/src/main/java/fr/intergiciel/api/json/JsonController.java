3248ea3a91b84c20643926b3c38baa5274db2b8c
Elena <elenabeylat@gmail.com>
2024-03-14 15:53:05 +0100

    feat : pr1 to cs1

--- java_apps/api/src/main/java/fr/intergiciel/api/json/JsonController.java
+++ java_apps/api/src/main/java/fr/intergiciel/api/json/JsonController.java
@@ -26,90 +26,10 @@ public class JsonController {
     @PostMapping("/send")
     public String sendMessage(@RequestBody String jsonMessage) {
         System.out.println("Post read json" + jsonMessage);
-//        System.out.println("Post read xml");
-//        System.out.println(xmlMessage);
-//
-//        // Créer une instance de XmlMapper pour lire le XML dans une structure de données générique
-//        XmlMapper xmlMapper = new XmlMapper();
-//        JsonNode mainNode = null;
-//        try {
-//            mainNode = xmlMapper.readTree(xmlMessage.getBytes());
-//        } catch (IOException e) {
-//            throw new RuntimeException(e);
-//        }
-//
-//        // Créer une instance de ObjectMapper pour convertir la structure de données générique en JSON
-//        ObjectMapper objectMapper = new ObjectMapper();
-//        String json = null;
-//        try {
-//            json = objectMapper.writerWithDefaultPrettyPrinter().writeValueAsString(mainNode);
-//        } catch (JsonProcessingException e) {
-//            throw new RuntimeException(e);
-//        }
-//        System.out.println("Json:" + json);
-//
-//
-//        // Création d'une patient
-//        ObjectNode patient = objectMapper.createObjectNode();
-//        // Récuperation des informations
-//
-//        JsonNode pid5Array = mainNode.path("PID").path("PID.5");
-//
-//        String birthName = null;
-//        String legalName = null;
-//
-//        if (pid5Array.isArray()) {
-//            for (JsonNode node : pid5Array) {
-//                String nameType = node.path("PID.5.7").asText();
-//                if ("D".equals(nameType)) {
-//                    birthName = node.path("PID.5.1").asText();
-//                } else if ("L".equals(nameType)) {
-//                    legalName = node.path("PID.5.1").asText();
-//                }
-//            }
-//        }
-//
-//        // Traiter les informations selon les cas
-//        System.out.println("Nom de naissance: " + birthName);
-//        System.out.println("Nom légal: " + legalName);
-//        // Ajout des informations dans la patient
-//        if (birthName != null) {
-//            patient.put("BirthName", birthName);
-//        }
-//        if (legalName != null) {
-//            patient.put("LegalName", legalName);
-//        }
-//
-//        System.out.println(patient);
-
-//        patient.put("BirthDate", mainNode.path("PID").path("PID.7").path("PID.7.1"));
-//        patient.put("FirstName", mainNode.path("PID").path("PID.5").path("PID.5.2"));
-//        patient.put("Prefix", mainNode.path("PID").path("PID.5").path("PID.5.5"));
-//        patient.put("ID", mainNode.path("PID").path("PID.3").path("PID.3.1"));
-//
-//        System.out.println(patient);
-//
-//        ObjectNode movements = objectMapper.createObjectNode();
-//
-//        movements.put("Service", mainNode.path("PV1").path("PV1.3").path("PV1.3.1"));
-//        movements.put("Room", mainNode.path("PV1").path("PV1.3").path("PV1.3.2"));
-//        movements.put("Bed", mainNode.path("PV1").path("PV1.3").path("PV1.3.3"));
-//
-//        ObjectNode stay = objectMapper.createObjectNode();
-//
-//        stay.put("NumUniqueSejour",mainNode.path("PV1").path("PV1.19").path("PV1.19.1"));
-//        stay.put("AdmiDate",mainNode.path("PV1").path("PV1.44").path("PV1.44.1"));
-//        stay.put("DeschargeDate",mainNode.path("PV1").path("PV1.45").path("PV1.45.1"));
-//
-//        ObjectNode jsonFinal = objectMapper.createObjectNode();
-//        jsonFinal.put("Patient", patient);
-//        jsonFinal.put("Movements", movements);
 
         // Retourner le JSON généré
         messageProducer.sendMessage("topic1", jsonMessage);
-//        return "Message sent: " + jsonFinal;
-//        return "Message sent: " + mainNode;
+        System.out.println("Done");
         return "sent : "+ jsonMessage;
-
     }
 }
--- java_apps/dbingest/src/main/java/fr/intergiciel/dbingest/DBIngestApp.java
+++ java_apps/dbingest/src/main/java/fr/intergiciel/dbingest/DBIngestApp.java
@@ -1,17 +1,37 @@
 package fr.intergiciel.dbingest;
 
+import org.apache.kafka.clients.consumer.ConsumerConfig;
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.time.Duration;
+import java.util.Collections;
+import java.util.Properties;
+
 public class DBIngestApp {
 
     public static void main(String[] args) {
+        Properties props = new Properties();
+        props.put(ConsumerConfig.BOOTSTRAP_SERVERS_CONFIG, "broker:29092");
+        props.put(ConsumerConfig.GROUP_ID_CONFIG, "group");
+        props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+        props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, "org.apache.kafka.common.serialization.StringDeserializer");
+
 
         System.out.println("Hello, World!");
+
+        KafkaConsumer<String, String> consumer = new KafkaConsumer<>(props);
+        consumer.subscribe(Collections.singletonList("topic1"));
+
+        System.out.println("Subscribe to topic");
         while (true) {
-            try {
-                Thread.sleep(1000);
-            } catch (InterruptedException e) {
-                e.printStackTrace();
+            ConsumerRecords<String, String> records = consumer.poll(Duration.ofMillis(1000));
+            for (ConsumerRecord<String, String> record : records) {
+                System.out.printf("offset = %d, key = %s, value = %s%n", record.offset(), record.key(), record.value());
+                // Traitez le message JSON ici
             }
+            consumer.commitSync();
         }
     }
-
 }
